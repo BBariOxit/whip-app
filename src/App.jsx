37640aec670b1167c28d5f7ae34f83faab2e1225
phanthaibao <phanbao2648@gmail.com>
2025-12-01 19:27:41 +0700

    change themeProvider into  CssVarsProvider/ extendTheme

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
 import Button from '@mui/material/Button'
 import AccessAlarmIcon from '@mui/icons-material/AccessAlarm'
 import ThreeDRotation from '@mui/icons-material/ThreeDRotation'
+import {
+  Experimental_CssVarsProvider as CssVarsProvider,
+  experimental_extendTheme as extendTheme,
+  useColorScheme
+} from '@mui/material/styles'
+
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme()
+  return (
+    <Button
+      onClick={() => {
+        setMode(mode === 'light' ? 'dark' : 'light')
+      }}
+    >
+      {mode === 'light' ? 'Turn dark' : 'Turn light'}
+    </Button>
+  )
+}
 
 function App() {
 
   return (
     <>
+      <ModeToggle />
       <div>baophan</div>
       <Button variant="text">Text</Button>
       <Button variant="contained">Contained</Button>
