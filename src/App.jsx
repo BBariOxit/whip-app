eff7f258e6248e1e5a9e6a1862851632a2f66f8f
phanthaibao <phanbao2648@gmail.com>
2025-12-08 21:01:08 +0700

    handle mode select with useColorScheme

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,47 @@ import {
   experimental_extendTheme as extendTheme,
   useColorScheme
 } from '@mui/material/styles'
-import useMediaQuery from '@mui/material/useMediaQuery';
+import useMediaQuery from '@mui/material/useMediaQuery'
+
+import Box from '@mui/material/Box'
+import InputLabel from '@mui/material/InputLabel'
+import MenuItem from '@mui/material/MenuItem'
+import FormControl from '@mui/material/FormControl'
+import Select from '@mui/material/Select'
+
+function ModeSelect() {
+  const { mode, setMode } = useColorScheme()
+  const handleChange = (event) => {
+    // setAge(event.target.value)
+    const selectedMode = event.target.value
+    setMode(selectedMode)
+  }
+
+  return (
+    <Box sx={{ minWidth: 120 }}>
+      <FormControl sx={{ minWidth: 150 }}>
+        <InputLabel id= "label-select-dark-light-mode">mode</InputLabel>
+        <Select
+          labelId="label-select-dark-light-mode"
+          id="select-dark-light-mode"
+          value={mode}
+          label="mode"
+          onChange={handleChange}
+        >
+          <MenuItem value="light">Light</MenuItem>
+          <MenuItem value="dark">Dark</MenuItem>
+          <MenuItem value="system">System</MenuItem>
+        </Select>
+      </FormControl>
+    </Box>
+  )
+}
 
 function ModeToggle() {
   const { mode, setMode } = useColorScheme()
-  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)')
-  const prefersLightMode = useMediaQuery('(prefers-color-scheme: light)')
+
+  // const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)')
+  // const prefersLightMode = useMediaQuery('(prefers-color-scheme: light)')
   return (
     <Button
       onClick={() => {
@@ -27,6 +62,9 @@ function App() {
 
   return (
     <>
+      <hr></hr>
+      <ModeSelect/>
+      <hr></hr>
       <ModeToggle />
       <div>baophan</div>
       <Button variant="text">Text</Button>
