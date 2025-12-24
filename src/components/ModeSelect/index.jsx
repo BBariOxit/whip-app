bf19a2cb2a53c6ead55c8b1d041074f2c5f2706f
phanthaibao <phanbao2648@gmail.com>
2025-12-24 20:14:00 +0700

    move ModeSelect into Components folder

---
+++ src/components/ModeSelect/index.jsx
@@ -0,0 +1,55 @@
+import {
+  useColorScheme
+} from '@mui/material/styles'
+
+import Box from '@mui/material/Box'
+import FormControl from '@mui/material/FormControl'
+import InputLabel from '@mui/material/InputLabel'
+import MenuItem from '@mui/material/MenuItem'
+import Select from '@mui/material/Select'
+
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined'
+import LightModeIcon from '@mui/icons-material/LightMode'
+import SettingsBrightnessIcon from '@mui/icons-material/SettingsBrightness'
+
+function ModeSelect() {
+  const { mode, setMode } = useColorScheme()
+  const handleChange = (event) => {
+    // setAge(event.target.value)
+    const selectedMode = event.target.value
+    setMode(selectedMode)
+  }
+
+  return (
+    <Box sx={{minWidth: 120}}>
+      <FormControl sx={{ minWidth: 150, m: 1 }} size='small'>
+        <InputLabel id= "label-select-dark-light-mode">mode</InputLabel>
+        <Select
+          labelId="label-select-dark-light-mode"
+          id="select-dark-light-mode"
+          value={mode}
+          label="mode"
+          onChange={handleChange}
+        >
+          <MenuItem value="light">
+            <div style={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
+              <LightModeIcon fontSize='small'/> Light
+            </div>
+          </MenuItem>
+          <MenuItem value="dark">
+            <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+              <DarkModeOutlinedIcon fontSize='small'/> Dark
+            </Box>
+          </MenuItem>
+          <MenuItem value="system">
+            <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+              <SettingsBrightnessIcon fontSize='small'/> System
+            </Box>
+          </MenuItem>
+        </Select>
+      </FormControl>
+    </Box>
+  )
+}
+
+export default ModeSelect
