047b8252175d7e8b7e06473ffd8acde5f61f7e57
phanthaibao <phanbao2648@gmail.com>
2025-12-26 15:05:36 +0700

    create drop down menu workspaces(base)

--- src/components/AppBar/index.jsx
+++ src/components/AppBar/index.jsx
@@ -3,21 +3,25 @@ import Box from '@mui/material/Box'
 import AppsIcon from '@mui/icons-material/Apps'
 import { ReactComponent as trelloLogo } from '~/assets/trello.svg'
 import SvgIcon from '@mui/material/SvgIcon'
+import Typography from '@mui/material/Typography'
+import Workspaces from './Menu/Workspaces'
 
 function AppBar() {
   return (
-    <Box
-      sx={{
-        width: '100%',
-        height: (theme) => theme.trello.appBarHeight,
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'space-between'
-      }}
-    >
-      <Box>
+    <Box px={2} sx={{
+      width: '100%',
+      height: (theme) => theme.trello.appBarHeight,
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'space-between'
+    }}>
+      <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
         <AppsIcon sx={{ color: 'primary.main' }}/>
-        <SvgIcon component={trelloLogo} inheritViewBox />
+        <Box sx={{ display: 'flex', alignItems: 'center', gap: 0.5 }}>
+          <SvgIcon component={trelloLogo} inheritViewBox sx={{ color: 'primary.main' }}/>
+          <Typography variant='span' sx={{ fontSize: '1.2rem', fontWeight: 'bold', color: 'primary.main' }}>Trello</Typography>
+        </Box>
+        <Workspaces />
       </Box>
 
       <Box>
