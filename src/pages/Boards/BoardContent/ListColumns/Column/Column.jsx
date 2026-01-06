b07be2f41f0326001c07d831c5b630065e2914f2
phanthaibao <phanbao2648@gmail.com>
2026-01-06 21:52:40 +0700

    integrate touch sensors and mouse sensor

--- src/pages/Boards/BoardContent/ListColumns/Column/Column.jsx
+++ src/pages/Boards/BoardContent/ListColumns/Column/Column.jsx
@@ -28,6 +28,7 @@ function Column({ column }) {
     data: { ...column }
   })
   const dndKitColumnStyles = {
+    touchAction: 'none',
     // Nếu sử dụng CSS.Transform như docs sẽ lỗi kiểu stretch
     // https://github.com/clauderic/dnd-kit/issues/117
     transform: CSS.Translate.toString(transform),
