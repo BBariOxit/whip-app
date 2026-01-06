b05a9d2a7d0387153cd5cb9299a546e0d2be846e
phanthaibao <phanbao2648@gmail.com>
2026-01-06 19:41:58 +0700

    refactor: add arrayMove logic for ordered columns in Board component

--- src/pages/Boards/BoardContent/BoardContent.jsx
+++ src/pages/Boards/BoardContent/BoardContent.jsx
@@ -1,12 +1,35 @@
 import Box from '@mui/material/Box'
 import ListColumns from './ListColumns/ListColumns'
 import { mapOrder } from '~/utils/sorts'
-import {DndContext} from '@dnd-kit/core'
+import { DndContext } from '@dnd-kit/core'
+import { arrayMove } from '@dnd-kit/sortable'
+import { useState, useEffect } from 'react'
 
 function BoardContent({ board }) {
-  const oderedColumns = mapOrder(board?.columns, board?.columnOrderIds, '_id')
+  const [orderedColumns, setOrderedColumns] = useState([])
+  useEffect(() => {
+    const oderedColumns = mapOrder(board?.columns, board?.columnOrderIds, '_id')
+    setOrderedColumns(oderedColumns)
+  }, [board])
+
   const handleDragEnd = (e) => {
     console.log('handleDragEnd:', e)
+    const { active, over } = e
+
+    if (active.id != over.id) {
+      //lấy vị trí cũ (từ thằng active)
+      const oldIndex = orderedColumns.findIndex((c) => c._id === active.id)
+      //lấy vị trí mới (từ thằng over)
+      const newIndex = orderedColumns.findIndex((c) => c._id === over.id)
+
+      const dndOrderedColumns = arrayMove(orderedColumns, oldIndex, newIndex)
+      // 2 cái log dữ liệu này sau để gọi API
+      // const dndOrderedColumnsIds = dndOrderedColumns.map(c => c._id)
+      // console.log('dndOrderedColumns: ', dndOrderedColumns)
+      // console.log('dndOrderedColumnsIds: ', dndOrderedColumnsIds)
+
+      setOrderedColumns(dndOrderedColumns)
+    }
   }
   return (
     <DndContext onDragEnd={handleDragEnd}>
@@ -17,7 +40,7 @@ function BoardContent({ board }) {
           height: (theme) => theme.trello.boardContentHeight,
           p: '10px 0'
         }}>
-        <ListColumns columns={oderedColumns}/>
+        <ListColumns columns={orderedColumns}/>
       </Box>
     </DndContext>
   )
