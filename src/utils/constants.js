e42ec398b8b46acf8b258a463466b7e4c2868483
phanthaibao <phanbao2648@gmail.com>
2026-03-01 21:35:09 +0700

    fix BUILDE_MODE dev and production

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,10 @@
 //chứa các biến , api endpoint api root
-export const API_ROOT = 'https://whip-api.onrender.com'
+let apiRoot = ''
+if (process.env.BUILD_MODE === 'dev') {
+  apiRoot = 'http://localhost:2008'
+}
+if (process.env.BUILD_MODE === 'production') {
+  apiRoot = 'https://whip-api.onrender.com'
+}
+console.log(apiRoot)
+export const API_ROOT = apiRoot
